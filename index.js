d05c2efa4823ba630307da658a9fb63cea5fa55f
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-08-12 10:20:42 +0530

    added book schema

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const morgan = require('morgan')
 const app = express()
 app.use(morgan('dev'))
 
+
+app.all("*", (req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: "Resource Not Found"
+    })
+})
+
+
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Listening to server on port: ${PORT}`.yellow)
---
+++ models/Category.js
@@ -0,0 +1,32 @@
+const mongoose = require('moongoose')
+
+const bookSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name for the book"]
+    },
+    description: {
+        type: String,
+        required: [true, "Please provide the description for the book"]
+    },
+    genre: {
+        type: String,
+        required: [true, "Please provide a genre for the book"]
+    },
+    status: {
+        type: String,
+        enum: ["draft", "review", "public"],
+        default: "draft",
+        required: [true, "Please provide a status"]
+    },
+    allowDownload: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+})
+
+const Book = mongoose.model('Book', bookSchema)
+
+module.exports = Book
