09a3fa2ff1fe5b75b4b2af424280c91ee051ce6f
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-08-27 20:18:58 +0530

    minor fix

--- controllers/books.js
+++ controllers/books.js
@@ -26,25 +26,14 @@ exports.getBook = asyncHandler(async (req, res, next) => {
     })
 })
 
+exports.createBook = asyncHandler(async (req, res, next) => {
+    const book = await Book.create(req.body);
+    res.status(200).json({
+        success: true,
+        book
+    })
+})
 
-// exports.createBook = asyncHandler(async (req, res, next) => {
-//     const book = await Book.create(req.body);
-//     res.status(200).json({
-//         success: true,
-//         book
-//     })
-// })
-
-exports.createBook = async(req, res, next) => {
-    try {
-        const book = await Book.create(req.body)
-        res.status(200).json({success: true, book})
-    } catch (error) {
-        console.log("Book error");
-        
-        next(error)
-    }
-}
 
 exports.updateBook = asyncHandler(async (req, res, next) => {
 
